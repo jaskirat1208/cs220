e7f253b51b64acc74a902e5010f7d879355e8477
jaskirat1208 <jaskiratsingh1208@gmail.com>
2017-04-05 14:39:55 +0530

    assign3_1

--- verilog_3/alu_four/alu_four.v
+++ verilog_3/alu_four/alu_four.v
@@ -2,10 +2,11 @@ module test;
 reg [3:0] a,b;
 wire [3:0]res;
 reg [1:0] opcode;
-alu_four alu(opcode,a,b,res);
+wire cf,sf,zf;
+alu_four alu(opcode,a,b,res,cf,sf,zf);
 initial begin
 	opcode=2'b00;
-	a=4'b1101;
+	a=4'b1001;
 	b=4'b1010;
 	$monitor(res);
 	#100
@@ -27,13 +28,15 @@ endmodule
 module alu_four(
 		input wire [1:0] opcode,
 		input wire [3:0] a,b,
-		output wire [3:0] result
+		output wire [3:0] result,
+		output wire cf,sf,zf
 	);
 	reg [3:0]res;
 	//opcodes: 00 : and, 01 : or , 10 : add, 01 : sub
 	always @(*) begin
 		if (opcode==2'b00) 	begin
 			res = a & b;	
+			
 		end 
 		if (opcode==2'b01) 	begin
 			res = a | b;	
@@ -44,6 +47,9 @@ module alu_four(
 		if (opcode==2'b11) 	begin
 			res = a - b;	
 		end 
+		if (res==4'b0000) begin
+				assign zf = 1;
+		end
 		
 	end
 	assign result = res;
